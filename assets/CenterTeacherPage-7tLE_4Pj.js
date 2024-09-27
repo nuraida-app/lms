62fc1582bc24adc675ffac97bc4b66e5c88b63d0
nuraida-app <nuraida.appdev@gmail.com>
2024-09-27 15:08:05 +0700

    Add center page

---
+++ assets/CenterTeacherPage-7tLE_4Pj.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-B4oIbNwU.js";const t=()=>e.jsx("div",{children:"CenterTeacherPage"});export{t as default};
---
+++ assets/useTheme-C3ddHqAM.js
@@ -0,0 +1,1 @@
+import{b7 as s,b5 as t,b4 as m}from"./index-B4oIbNwU.js";function a(){const e=s(m);return e[t]||e}export{a as u};
