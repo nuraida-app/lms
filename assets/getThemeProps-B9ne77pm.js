884ef9d152096056c2c49a15945a9acf91b67ec3
nuraida-app <nuraida.appdev@gmail.com>
2024-10-29 07:55:03 +0700

    Remove required form

--- assets/getThemeProps-B9ne77pm.js
+++
@@ -1,1 +0,0 @@
-import{aU as r}from"./index-DPg1yMmz.js";function p(t){const{theme:o,name:e,props:n}=t;return!o||!o.components||!o.components[e]||!o.components[e].defaultProps?n:r(o.components[e].defaultProps,n)}export{p as g};
